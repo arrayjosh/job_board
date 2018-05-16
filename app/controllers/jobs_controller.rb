48fe0b5e8f8101f4fbc7ae1dcc36f9296bbec623
Josh Sanderlin <jsanderlin@arrayschool.com>
2018-05-16 09:31:04 -0600

    after tuesday lecture

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -7,4 +7,24 @@ class JobsController < ApplicationController
   def new
     @job = Job.new
   end
+
+  def create
+    @job = Job.new(job_params)
+    # @job.title = params[:job][:title]
+    # @job.description = params[:job][:description]
+
+    if @job.save
+      redirect_to jobs_path
+    else
+      # debugger
+      flash.now[:notice] = "There's problems with your new job form"
+      render 'new'
+    end
+    # debugger # this is a breakpoint, the server will stop when it hits this, and let us debug
+  end
+
+  private
+    def job_params
+      params.require(:job).permit(:title, :description)
+    end
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,4 @@
 class Job < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 5 }
+  validates :description, presence: true, length: { minimum: 20 }
 end
